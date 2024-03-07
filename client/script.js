ccd286bdef6bf0a9562ac7d961b0cc847881c771
Ashish Kumar <myselfashish777@gmail.com>
2024-03-07 12:43:30 +0530

    updated Url

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://chat-api-mzpj.onrender.com/', {
+    const response = await fetch('http://localhost:5001/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
